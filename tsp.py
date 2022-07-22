a4d741c4e7c43d1a122d619b3f3e1e3a5710342d
Wesley <wesley.melo@jobconvo.com>
2022-07-21 22:20:18 -0300

    multistart

--- tsp.py
+++ tsp.py
@@ -8,8 +8,9 @@ from rand import rand_num
 def greedy_closest(distance_matrix, tour):
     closest = 9999
     if len(tour) == 0:
+        #last_city = rand_num(len(distance_matrix[0]))
         last_city = 1
-        tour.append(1)
+        tour.append(last_city)
     else:
         last_city = tour[-1]
 
@@ -21,8 +22,9 @@ def greedy_closest(distance_matrix, tour):
     try:
         tour.append(next_city + 1)
     except:
-        print('---- fim')
+        pass
     return tour
+
 def cheapestInsertion(distance_matrix):
     dista = distance_matrix
     tour = []
@@ -91,18 +93,25 @@ def localsearch_2opt(distance_matrix, city_tour):
             best_route = copy.deepcopy(seed)
     return tour
 
-def grasp(distance_matrix, n):
+def grasp(distance_matrix, n, iterations= 50):
     tour = []
     city_tour = []
+    best_route = [[9999],9999]
+    
     print('---- Montando solução')
-    for i in range(0,n):
-        print('---- Tour atual{}'.format(tour))
-        tour = greedy_closest(distance_matrix,tour)
-    city_tour.append(tour)
-    city_tour.append(0)
-    city_tour[1] = (get_total_distance(distance_matrix,city_tour[0]))
-    best_route = localsearch_2opt(distance_matrix,city_tour)
-    print(best_route)
+    for count in range(0,iterations): # Multistart
+        # construtor
+        for i in range(0,n):
+            tour = greedy_closest(distance_matrix,tour)
+        city_tour.append(tour)
+        city_tour.append((get_total_distance(distance_matrix,city_tour[0])))
+        actual_best_route = localsearch_2opt(distance_matrix,city_tour)
+        if actual_best_route[1] < best_route[1]:
+            best_route = copy.deepcopy(actual_best_route)
+        #reset tour
+        print("Iteration =", count, "-> Distance =", best_route[1])
+        actual_best_route = []
+        tour = []
 
 def grasp2(distance_matrix, n):
     tour = []
@@ -124,7 +133,8 @@ if __name__ == '__main__':
         '-f', '--file', help='path to file of distance matrix', metavar='PATH', required=True)
     args = parser.parse_args()
 
-    #m = read_distance_matrix(args.file)
-    #grasp(m, 5)
-    mat=pegaMatriz(args.file)
-    grasp2(mat, 6)
+    m = read_distance_matrix(args.file)
+    n = len(m[0])
+    grasp(m, n)
+    #mat=pegaMatriz(args.file)
+    #grasp2(mat, 6)
